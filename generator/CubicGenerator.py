7e122195fe0171aa059f86691db7d8526100a645
Laptop <s27756@pjwstk.edu.pl>
2024-03-18 13:11:25 +0100

    task 9 - override method

--- generator/CubicGenerator.py
+++ generator/CubicGenerator.py
@@ -10,6 +10,16 @@ class CubicGenerator(SquareGenerator):
 
         return [x**3 for x in range(start, end+1)]
 
+    def generate_squares(self, start, end):
+        if end < start:
+            raise Exception("the end of the range is greater than the start")
+
+        return [x**2 for x in range(start, end + 1)]
+
 generator = CubicGenerator()
 cubics = generator.generate_cubes(1, 10)
 print(cubics)
+
+generator = SquareGenerator()
+squares2 = generator.generate_squares(1, 10)
+print(squares2)
