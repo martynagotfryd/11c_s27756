6fd1f7314124117a9e70d9c5aa7f29e97cff28c3
Laptop <s27756@pjwstk.edu.pl>
2024-03-18 11:10:17 +0100

    task 2 - method

--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
-#1
-squares = [x**2 for x in range(1, 11)]
+#2
+
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end + 1)]
+
+start = 1
+end = 10
+squares = generate_squares(start, end)
 print(squares)
