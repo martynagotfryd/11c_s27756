839c07eea3ee0a98e555817e264a87fc1010c973
Laptop <s27756@pjwstk.edu.pl>
2024-03-18 11:53:21 +0100

    task 3 - class

--- Main.py
+++ Main.py
@@ -7,3 +7,16 @@ start = 1
 end = 10
 squares = generate_squares(start, end)
 print(squares)
+
+#3
+class SquareGenerator:
+    def __init__(self):
+        pass
+
+    def generate_squares(self, start, end):
+
+        return [x**2 for x in range(start, end + 1)]
+
+generator = SquareGenerator()
+squares = generator.generate_squares(1, 10)
+print(squares)
