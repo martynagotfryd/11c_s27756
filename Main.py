de06329e775704ac528ed7d46108b9ac21176a0d
Laptop <s27756@pjwstk.edu.pl>
2024-03-18 12:01:49 +0100

    task 5 - exception added to task 3

--- Main.py
+++ Main.py
@@ -9,17 +9,19 @@ end = 10
 squares = generate_squares(start, end)
 print(squares)
 
-#3
+#3 and 5
 class SquareGenerator:
     def __init__(self):
         pass
 
     def generate_squares(self, start, end):
+        if end < start:
+            raise Exception("the end of the range is less than the start")
 
         return [x**2 for x in range(start, end + 1)]
 
 generator = SquareGenerator()
-squares = generator.generate_squares(1, 10)
+squares = generator.generate_squares(10, 1)
 print(squares)
 
 #4
